56a1123162c0ac85a994fa330a31dbb82b747f0b
leviwani <leviwani03@gmail.com>
2025-03-11 13:16:10 +0530

    init commit

---
+++ src/test/java/com/example/Backend_Traini8_LeviManaseWani/BackendTraini8LeviManaseWaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Traini8_LeviManaseWani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTraini8LeviManaseWaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
